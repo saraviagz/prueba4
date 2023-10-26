b29a4e037c58b79d14bc4fc7c553e182511b0c33
saraviagz <alex.saravia@unmsm.edu.pe>
2023-10-26 16:27:29 -0500

    iniciando el proyecto de ejemplo

---
+++ src/main/java/pe/edu/unmsm/pp20231026/Pp20231026.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package pe.edu.unmsm.pp20231026;
+
+/**
+ *
+ * @author laboratorio-fisi
+ */
+public class Pp20231026 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
